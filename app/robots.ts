802d4cd402189b893f38806e8514d43a29d7a735
krystian-png <krystian@croney.io>
2025-08-26 14:49:46 +0200

    Use site URL env in robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,14 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const BASE_URL =
-    process.env.NEXT_PUBLIC_SITE_URL || "https://www.zmianakrs.pl";
+  const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+
+  if (!BASE_URL) {
+    throw new Error("NEXT_PUBLIC_SITE_URL env variable is not set");
+  }
 
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: `${BASE_URL}/sitemap.xml`,
+    sitemap: `${BASE_URL.replace(/\/$/, "")}/sitemap.xml`,
   };
 }
