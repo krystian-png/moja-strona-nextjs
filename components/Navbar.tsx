e7597a9f6849e5363e8bf9a5b560530c7bfbb23d
krystian-png <krystian@croney.io>
2025-08-26 23:39:16 +0200

    chore: update contact phone number

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,8 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto px-4 py-2 flex flex-col sm:flex-row justify-between items-center gap-2">
         <ul className="flex flex-col sm:flex-row items-center gap-4 text-gray-600">
           <li>
-            <a href="tel:+48111222333" className="hover:text-gray-900">
-              +48 111 222 333
+            <a href="tel:572234779" className="hover:text-gray-900">
+              572 234 779
             </a>
           </li>
           <li>
